477a5011356260792a2884fb51dda0c6119afd67
Jakub Klimczewski <kukli8@o2.pl>
2022-11-23 15:43:03 +0100

    lab05#firstcommit

---
+++ lab05/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import SignupForm from './formik';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <SignupForm />
+);
+
